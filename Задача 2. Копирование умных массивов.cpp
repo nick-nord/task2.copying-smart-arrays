edc73113f7374d25147f7f1bb7b31661206faeeb
Nick-nord <kiseleff.nikol@gmail.com>
2026-03-12 20:04:13 +0300

    доработка

--- Задача 2. Копирование умных массивов.cpp
+++ Задача 2. Копирование умных массивов.cpp
@@ -65,12 +65,18 @@ public:
         if (index >= size) {
             throw std::out_of_range("Index out of range");
         }
+        else if (index < 0) {
+            throw std::out_of_range("Index should not be negative");
+        }
         return arr[index];
     }
 
     const int& operator[](int index) const {
         if (index >= size) {
             throw std::out_of_range("Index out of range");
+        }
+        else if (index < 0) {
+            throw std::out_of_range("Index should not be negative");
         }
         return arr[index];
     }
